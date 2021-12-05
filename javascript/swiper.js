fd4c4960f82495eac8fa647df9d3fd296b54f39f
Hugo Corrêa <hugodemorais@live.com>
2021-12-05 18:02:23 -0300

    att

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -14,11 +14,6 @@ const swiper = new Swiper(".swiper1", {
     prevEl: ".swiper-button-prev",
   },
 
-  //Autoplay
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: true,
-  },
 });
 
 
@@ -27,4 +22,24 @@ var swiper2 = new Swiper(".swiper2", {
   grabCursor: true,
   loop: true,
 /*       initialSlide: 2, */
+});
+
+
+
+const swiper3 = new Swiper(".swiper3", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination3",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3",
+  },
+
 });
