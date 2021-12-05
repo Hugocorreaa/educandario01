d1451eb261d8707189158d8c18dac0706bc5f298
Hugo Corrêa <hugodemorais@live.com>
2021-12-05 13:48:33 -0300

    att

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,21 +1,30 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const swiper = new Swiper(".swiper1", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //Autoplay
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: true,
+  },
+});
+
+
+var swiper2 = new Swiper(".swiper2", {
+  effect: "cards",
+  grabCursor: true,
+  loop: true,
+/*       initialSlide: 2, */
+});
