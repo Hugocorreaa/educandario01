b3c2b4dbd0a0db3b44980fcf5ba41bf42d176765
Hugo Corrêa <hugodemorais@live.com>
2021-12-22 15:16:14 -0300

    att

---
+++ javascript/eventos/swiperEventos.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 1,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
